530af9aa7742c75bc03cf171cc4944666c919324
Любомир Лалов <ldlalov@hotmail.com>
2022-03-15 13:17:18 +0200

    Add project files.

---
+++ 02. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\+359( |-)2\1[0-9]{3}\1[0-9]{4}\b");
+            string phones = Console.ReadLine();
+            MatchCollection matches = regex.Matches(phones);
+            var matchedPhones = matches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+                Console.Write(string.Join(", ",matches));
+        }
+    }
+}
